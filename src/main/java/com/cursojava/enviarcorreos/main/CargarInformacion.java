056ca7b05b48b92febb4405824d5e44a3fb94d0a
Cristian Aguilar <cristian.aguilar02@gmail.com>
2021-08-26 21:37:33 -0600

    Crear la informacion del cliente predeterminada

---
+++ src/main/java/com/cursojava/enviarcorreos/main/CargarInformacion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cursojava.enviarcorreos.main;
+
+import com.cursojava.enviarcorreos.entities.Cliente;
+import com.cursojava.enviarcorreos.entities.Departamento;
+import com.cursojava.enviarcorreos.entities.Municipio;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Aguilar
+ */
+public class CargarInformacion {
+    
+    // listas para cargar la informacion
+    private List<Departamento> listDeptos = new ArrayList<>();
+    private List<Municipio> listMunis = new ArrayList<>();
+    private List<Cliente> listCliente = new ArrayList<>();
+    
+    // metodo para procesar la informacion
+    
+    
+}
